4105a5b685bcc83d4662eaf652421a680e141806
Michael Szeto <szetomj@hotmail.co.nz>
2016-12-11 17:54:16 +1300

    Added status verification to make sure request is successfully completed
    Added status verification to make sure request is successfully completed

--- js/ajax-requests.js
+++ js/ajax-requests.js
@@ -1,17 +1,20 @@
 var ajaxRequest = new XMLHttpRequest();
-var btnSubmit = document.getElementById("btnSubmit");
+var btnSubmit = document.querySelectorAll(".btnSubmit");
 var successBanner = document.querySelectorAll('.success');
 
 ajaxRequest.onreadystatechange = function(){
 
-	if(ajaxRequest.readyState === 4){
+	if(ajaxRequest.readyState === 4 && ajaxRequest.status === 200){
 		var banner = document.getElementById("banner");
 		banner.innerHTML = ajaxRequest.responseText;
+	}else if(ajaxRequest.readyState === 404){
+		alert("File is not found");
 	}
 };
 
-ajaxRequest.open("Get", "success.html");
+ajaxRequest.open("Get", "success.html", true);
 
 function loadBanner(){
 	ajaxRequest.send();
 }
+
